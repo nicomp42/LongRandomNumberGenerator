66da21f54c4733da0c0cc7b460b4a04450fd1970
Bill Nicholson <nicholdw@ucmail.uc.edu>
2019-02-18 12:08:04 -0500

    First Commit

--- LongRandomNumber/LongRandomNumber.cs
+++ LongRandomNumber/LongRandomNumber.cs
@@ -7,10 +7,6 @@
  * https://en.wikipedia.org/wiki/Miller%E2%80%93Rabin_primality_test
 */
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LongRandomNumber
 {
@@ -20,7 +16,7 @@ namespace LongRandomNumber
         /// Demonstrating the methods in here
         /// </summary>
         /// <param name="args"></param>
-       static void Main(string[] args)
+        static void Main(string[] args)
         {
             Console.WriteLine(GetLongPrime(false));
         }
@@ -33,12 +29,13 @@ namespace LongRandomNumber
         {
             long n1 = 0, n2 = 0, n3 = 0, random = 0;
             Random r = new Random();
-            while (true) {
+            while (true)
+            {
                 n1 = r.Next();
                 n2 = r.Next();
-                n3 = (long)(n1 << 31); 
+                n3 = (long)(n1 << 31);
                 random = Math.Abs(n3 + n2);
-                if (verbose) {Console.Write(n1 + "\n" + n2 + "\n" + n3 + "\n" + random); }
+                if (verbose) { Console.Write(n1 + "\n" + n2 + "\n" + n3 + "\n" + random); }
                 if (IsRandom(random)) break;
             }
             return random;
@@ -54,7 +51,8 @@ namespace LongRandomNumber
             if (number % 2 == 0) { return false; }
             long limit = (long)Math.Sqrt(number);
 
-            for (long i = 3; i <= limit; i+= 2) {
+            for (long i = 3; i <= limit; i += 2)
+            {
                 if (number % i == 0) { random = false; break; }
             }
             return random;
